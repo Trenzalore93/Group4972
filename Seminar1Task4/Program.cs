c9c0de4e2d98aa9d083db1bdcf6b286eebcae9c0
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-04 21:49:34 +0300

    Updates tasks 4 and 5

--- Seminar1Task4/Program.cs
+++ Seminar1Task4/Program.cs
@@ -1,18 +1,1 @@
-﻿// Задача №5
-// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-Console.WriteLine("Введите число: ");
-int numN = int.Parse(Console.ReadLine() ?? "0");
-int negNumN = (-1) * numN;
-while (negNumN < numN)
-{
-    Console.Write(negNumN + ", ");
-    negNumN++;
-}
-Console.WriteLine(numN);
-
-// int a = int.Parse(Console.ReadLine());
-// if (a < 0) a = -a;
-// for(int i = -a; i <= a; i++){
-//    Console.Write(i);
-//    Console.Write(" ");
-// }
+﻿
