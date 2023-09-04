13d93f8b908f1336bf4893985e5f4c0d5dbfc7f4
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-04 21:46:43 +0300

    Added task 4

--- Seminar1Task3/Program.cs
+++ Seminar1Task3/Program.cs
@@ -1,14 +1,19 @@
 ﻿// Задача №3
 // Напишите программу, которая будет выдавать название дня недели по заданному номеру.
 
+// Console.WriteLine("Введите порядковый номер дня недели: ");
+// int day = int.Parse(Console.ReadLine() ?? "0");
+//string[] dayOfWeek = new string[7];
+//dayOfWeek[0] = "Понедельник";
+///dayOfWeek[1] = "Вторник";
+//dayOfWeek[2] = "Среда";
+//dayOfWeek[3] = "Четверг";
+//dayOfWeek[4] = "Пятница";
+//dayOfWeek[5] = "Суббота";
+//dayOfWeek[6] = "Воскресенье";
+//Console.WriteLine(dayOfWeek[day - 1]);
+
 Console.WriteLine("Введите порядковый номер дня недели: ");
-int day = int.Parse(Console.ReadLine() ?? "0");
-string[] dayOfWeek = new string[7];
-dayOfWeek[0] = "Понедельник";
-dayOfWeek[1] = "Вторник";
-dayOfWeek[2] = "Среда";
-dayOfWeek[3] = "Четверг";
-dayOfWeek[4] = "Пятница";
-dayOfWeek[5] = "Суббота";
-dayOfWeek[6] = "Воскресенье";
-Console.WriteLine(dayOfWeek[day - 1]);
+int dayNum = int.Parse(Console.ReadLine() ?? "0");
+string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum));
+Console.WriteLine(outDayOfWeek);
