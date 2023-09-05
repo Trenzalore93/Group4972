c082402c7d0a0fd1c0b5b583920c520decac0871
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-05 22:08:46 +0300

    добавила решение домашнего задания Seminar1Task2

--- Seminar1Task2/Program.cs
+++ Seminar1Task2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+int max = numA;
+if (numB > numA)
+{
+    max = numB;
+}
+Console.WriteLine("Максимальное число: "+max);
