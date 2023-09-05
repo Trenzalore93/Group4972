d79e34dfe69bd878c63b3751a544b60f019ef933
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-05 22:25:26 +0300

    добавила решение домашнего задания Seminar1Task4

--- Seminar1Task4/Program.cs
+++ Seminar1Task4/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine() ?? "0");
+int[] array = {num1,num2,num3};
+int index = 1;
+int max = array[0];
+while (index<array.Length)
+{
+    if (array[index]>max)
+    {
+        max = array[index];
+    }
+    index++;
+}
+Console.WriteLine("Максимальное число: "+max);
