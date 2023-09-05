ee945c6eda34f46ac9dc270cae48dec0fcced63c
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-05 22:26:19 +0300

    добавила решение домашнего задания Seminar1Task2

--- Seminar1Task2/Program.cs
+++ Seminar1Task2/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
 int numA = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите второе число: ");
 int numB = int.Parse(Console.ReadLine() ?? "0");
