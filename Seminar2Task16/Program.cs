37258e9b9641c96af675b6863c120b2494fe147e
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-11 21:51:29 +0300

    Добавили решение sem2task16

---
+++ Seminar2Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+bool test1 = (numA == numB * numB);
+bool test2 = (numB == numA * numA);
+if (test1) Console.WriteLine("Первое число - квадрат второго.");
+if (test2) Console.WriteLine("Второе число - квадрат первого.");
+if (!(test1 || test2)) //логическое ИЛИ
+{
+    Console.WriteLine("Числа не являются квадратами друг друга.");
+}
