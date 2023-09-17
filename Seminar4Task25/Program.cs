74f11b87c70af3df931335e1716b303f2b0af67f
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-17 16:31:58 +0300

    Добавила решения домашнего задания 25

---
+++ Seminar4Task25/Program.cs
@@ -0,0 +1,81 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+// double RaiseToPow(int numA, int numB)
+// {
+//     double result = 0;
+//     result = Math.Pow(numA, numB);
+//     return result;
+// }
+
+// void PrintResult(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// int numA = ReadData("Введите число А: ");
+// int numB = ReadData("Введите степень - число B: ");
+// double numAtoB = RaiseToPow(numA, numB);
+// PrintResult("Число А в степени B равно "+numAtoB);
+
+
+// ______________________________________________________________________________________ 
+//(задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень    ||
+//_______________________________________________________________________________________|
+
+int ReadNum(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+string ReadOperation(string msg)
+{
+    Console.WriteLine(msg);
+    string operation = Console.ReadLine();
+    return operation;
+}
+
+bool CheckOper(string operation)
+{
+    bool noOper = false;
+    if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "в степени") noOper = false;
+    else noOper = true;
+    return noOper;    
+}
+
+double Calc(int numA, int numB, string operation)
+{
+    double result = 0;
+    if (operation == "+") result = numA + numB;
+    else if (operation == "-") result = numA - numB;
+    else if (operation == "/") result = numA / numB;
+    else if (operation == "*") result = numA * numB;
+    else if (operation == "в степени") result = Math.Pow(numA, numB);
+    return result;
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int numA = ReadNum("Введите число А: ");
+string operation = ReadOperation("Введите операцию: +, -, /, * или в степени");
+int numB = ReadNum("Введите число B: ");
+if (CheckOper(operation) == true) PrintResult("Операция не найдена");
+else
+{
+    double result = Calc(numA, numB, operation);
+    PrintResult($"{numA} {operation} {numB} = {result}");
+}
