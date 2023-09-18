9a6fb142c89455d43361cdb8e83230df60961a4a
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-18 19:42:31 +0300

    добавила комментарии к заданию 25

--- Seminar4Task25/Program.cs
+++ Seminar4Task25/Program.cs
@@ -3,29 +3,29 @@
 // 2, 4 -> 16
 
 
-// int ReadData(string msg)
+// int ReadData(string msg) // метод для введения чисел с клавиатуры
 // {
 //     Console.WriteLine(msg);
 //     int num = int.Parse(Console.ReadLine() ?? "0");
 //     return num;
 // }
 
-// double RaiseToPow(int numA, int numB)
+// double RaiseToPow(int numA, int numB) // возводим в степень
 // {
 //     double result = 0;
 //     result = Math.Pow(numA, numB);
 //     return result;
 // }
 
-// void PrintResult(string msg)
+// void PrintResult(string msg) // печатаем результат
 // {
 //     Console.WriteLine(msg);
 // }
 
 // int numA = ReadData("Введите число А: ");
-// int numB = ReadData("Введите степень - число B: ");
-// if (numB < 0) Console.WriteLine("Число B должно быть натуральным!");
-// else
+// int numB = ReadData("Введите степень - число B: "); // ввели оба числа
+// if (numB < 0) Console.WriteLine("Число B должно быть натуральным!"); // условие, чтобы проверить, является ли число B натуральным
+// else // если да - возводим в степень и печатаем ответ
 // {
 //     double numAtoB = RaiseToPow(numA, numB);
 //     PrintResult("Число А в степени B равно " + numAtoB);
@@ -37,50 +37,51 @@
 //(задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень    ||
 //_______________________________________________________________________________________|
 
-int ReadNum(string msg)
+int ReadNum(string msg) // вводим числа с клавиатуры
 {
     Console.WriteLine(msg);
     int num = int.Parse(Console.ReadLine() ?? "0");
     return num;
 }
 
-string ReadOperation(string msg)
+string ReadOperation(string msg) // вводим вид вычислений с клавиатуры (+, -, /, *, ^ (в степени))
 {
     Console.WriteLine(msg);
     string operation = Console.ReadLine();
     return operation;
 }
 
-bool CheckOper(string operation)
+bool CheckOper(string operation) // проверяем введённый символ для операции 
 {
     bool noOper = false;
-    if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "в степени") noOper = false;
+    if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "^") noOper = false;
     else noOper = true;
     return noOper;    
 }
 
-double Calc(int numA, int numB, string operation)
+double Calc(int numA, int numB, string operation) //метод для вычисления, исходя из введённого символа операции
 {
     double result = 0;
     if (operation == "+") result = numA + numB;
     else if (operation == "-") result = numA - numB;
     else if (operation == "/") result = numA / numB;
     else if (operation == "*") result = numA * numB;
-    else if (operation == "в степени") result = Math.Pow(numA, numB);
+    else if (operation == "^") result = Math.Pow(numA, numB);
     return result;
 }
 
-void PrintResult(string msg)
+void PrintResult(string msg) // метод для выведения результата
 {
     Console.WriteLine(msg);
 }
 
-int numA = ReadNum("Введите число А: ");
-string operation = ReadOperation("Введите операцию: +, -, /, * или в степени");
-int numB = ReadNum("Введите число B: ");
-if (CheckOper(operation) == true) PrintResult("Операция не найдена");
-else
+int numA = ReadNum("Введите число А: "); // ввели первое число
+string operation = ReadOperation("Введите операцию: +, -, /, * или ^ (в степени)"); // ввели символ операции
+int numB = ReadNum("Введите число B: "); // ввели второе число
+if (CheckOper(operation) == true) PrintResult("Операция не найдена"); // используем проверку символа и выводим сообщение, если он не верен
 {
-    double result = Calc(numA, numB, operation);
+    double result = Calc(numA, numB, operation); // если верен - выполняем операцию
     PrintResult($"{numA} {operation} {numB} = {result}");
 }
+
+//!!!!!! У меня почему-то с типом данных double не выводит дробные числа => 7 / 3 = 2.
