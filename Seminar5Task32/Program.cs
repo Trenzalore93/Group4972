5e754450eb18956e443c9b774519538f92c481f9
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-20 22:04:33 +0300

    added tasks 31,32,33

---
+++ Seminar5Task32/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] GenArray(int length, int min, int max) //заполняем массив случайными значениями от -9 до 9 с длиной 12
+{
+    int[] array = new int[12];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+void PrintArray(int[] array) // выводим массив
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
+void InverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+}
+
+int[] array = GenArray(12, -9, 9);
+PrintArray(array);
+InverseArray(array);
+Console.WriteLine("");
+PrintArray(array);
---
+++ Seminar5Task33/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int[] GenArray(int length, int min, int max) //заполняем массив случайными значениями от -9 до 9 с длиной 12
+{
+    int[] array = new int[12];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // выводим массив
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+    Console.WriteLine("");
+}
+
+int FindDigit(int[] array, int digit)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == digit) return i;
+    }
+    return -1;
+}
+
+int num = ReadData("Введите число: ");
+int[] arr = GenArray(12, -9, 9);
+PrintArray(arr);
+int answer = FindDigit(arr, num);
+if (answer == (-1)) Console.WriteLine($"Числа {num} нет в массиве");
+else Console.WriteLine($"Позиция числа {num} в массиве - {answer + 1}");
