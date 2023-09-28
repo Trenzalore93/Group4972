a41632d5dad46bfaa5c52833cae08bef9f088363
Trenzalore93 <last.waltz.fb@gmail.com>
2023-09-29 00:30:47 +0300

    Added tasks 41 43

---
+++ Seminar6Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// (Задание со звездочкой) Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+
+int[] ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    string initial = Console.ReadLine();
+    //string newString = initial.Replace(",", " ");
+    int[] numbers = initial.Split(',').Select(int.Parse).ToArray();
+    return numbers;
+}
+
+void PrintArray(int[] array) // выводим массив
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+    Console.WriteLine("");
+}
+
+int PositivityCheck(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] array = ReadData("Введите числа через запятую: ");
+PrintArray(array);
+int result = PositivityCheck(array);
+Console.WriteLine($"Количество чисел больше нуля - {result}");
+
---
+++ Seminar6Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    double num = double.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+void FindIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2) Console.WriteLine("Прямые параллельны и не пересекаются.");
+    else
+    {
+        double x = 0;
+        double y = 0;
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения прямых имеет координаты: x = {x}, y = {y}");
+    }
+}
+
+double b1 = ReadData("Введите значение b1: ");
+double k1 = ReadData("Введите значение k1: ");
+double b2 = ReadData("Введите значение b2: ");
+double k2 = ReadData("Введите значение k2: ");
+FindIntersectionPoint(b1, k1, b2, k2);
